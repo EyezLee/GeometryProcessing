fdfd5047137377c02935024155922cbabaa91e0c
Eyez <lmt18072747669@gmail.com>
2020-04-24 13:37:57 +0900

    Set up render window

--- GeometryProcessing/main.cpp
+++ GeometryProcessing/main.cpp
@@ -1,6 +1,56 @@
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// namespace 
+using namespace std;
+
+// ultilities variables
+int SCR_WIDTH = 800;
+int SCR_HEIGHT = 600;
+
+// ultilities func
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
-	return -1;
+	// config glfw window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// window object
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "GeometryProcessing", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// config glad function pointers of OpenGL
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to init GLAD" << endl;
+		return -1;
+	}
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// terminte program
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
