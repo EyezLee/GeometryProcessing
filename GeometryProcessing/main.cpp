937d1d30b113ca6f86fba565a76b1056575fcef6
Eyez <lmt18072747669@gmail.com>
2020-05-06 18:47:15 +0900

    Structed material and light in shader

--- GeometryProcessing/main.cpp
+++ GeometryProcessing/main.cpp
@@ -137,12 +137,12 @@ int main()
 	// set uniform values to shader program
 	shaderProgram.UseShader(); // need to install shader program object first
 	shaderProgram.SetMat4("projection", scene.projection);
-	shaderProgram.SetVec3("lightPos", scene.lights[0].pos);
-	shaderProgram.SetVec3("lightCol", scene.lights[0].col);
-	shaderProgram.SetVec3("diffuseCol", scene.models[0].material.diffuse);
-	shaderProgram.SetVec3("ambientCol", scene.models[0].material.ambient);
-	shaderProgram.SetVec3("specularCol", scene.models[0].material.specular);
-	shaderProgram.SetFloat("shininess", scene.models[0].material.shininess);
+	shaderProgram.SetVec3("light.lightPos", scene.lights[0].pos);
+	shaderProgram.SetVec3("light.lightCol", scene.lights[0].col);
+	shaderProgram.SetVec3("material.diffuseCol", scene.models[0].material.diffuse);
+	shaderProgram.SetVec3("material.ambientCol", scene.models[0].material.ambient);
+	shaderProgram.SetVec3("material.specularCol", scene.models[0].material.specular);
+	shaderProgram.SetFloat("material.shininess", scene.models[0].material.shininess);
 	shaderProgram.SetVec3("cameraPos", camera.m_position);
 	glm::mat3 modelNorm = glm::mat3(glm::transpose(glm::inverse(scene.models[0].modelMatrix)));
 	shaderProgram.SetMat3("modelNorm", modelNorm);
--- src/shaders/phongShading.fs
+++ src/shaders/phongShading.fs
@@ -3,13 +3,23 @@
 in vec3 normal;
 in vec3 FragPos;
 
-uniform vec3 diffuseCol;
-uniform vec3 ambientCol;
-uniform vec3 specularCol;
-uniform float shininess;
+struct Material
+{
+	vec3 diffuseCol;
+	vec3 ambientCol;
+	vec3 specularCol;
+	float shininess;
+};
 
-uniform vec3 lightCol;
-uniform vec3 lightPos;
+struct Light
+{
+	vec3 lightCol;
+	vec3 lightPos;
+};
+
+uniform Material material;
+uniform Light light;
+
 uniform vec3 cameraPos;
 
 out vec4 FragColor;
@@ -17,17 +27,17 @@ out vec4 FragColor;
 void main()
 {
 	// diffuse
-	vec3 lightDir = normalize(lightPos - FragPos);
+	vec3 lightDir = normalize(light.lightPos - FragPos);
 	float diff = max(dot(normal, lightDir), 0.0f);
-	vec3 diffuse = diff * diffuseCol;
+	vec3 diffuse = diff * material.diffuseCol;
 
 	// specular
 	vec3 lightRef = reflect(-lightDir, normal); // light relect
 	vec3 eyeDir = normalize(cameraPos - FragPos);
-	float spec = pow(max(dot(eyeDir, lightRef), 0.0f), 1/shininess);
-	vec3 specular = spec * specularCol;
+	float spec = pow(max(dot(eyeDir, lightRef), 0.0f), 1/material.shininess);
+	vec3 specular = spec * material.specularCol;
 
 	// phong shading formula
-	vec3 color = (ambientCol + diffuse + specular) * lightCol; 
+	vec3 color = (material.ambientCol + diffuse + specular) * light.lightCol; 
 	FragColor = vec4(color, 1.0f);
 }
